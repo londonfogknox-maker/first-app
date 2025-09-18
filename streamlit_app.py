9679ca6a3faed89ab3d6db189b05537c4e449398
londonfogknox-maker <londonfogknox@gmail.com>
2025-09-18 14:49:57 -0400

    Update streamlit_app.py
    Trying a slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("Welcome, this is a test page")
 st.write(
     "I use this page to explore what I can do with Streamlit."
 )
+
+x = st.slider('Get points by moving the slider!')
+st.write(x, ' points', x * x)
